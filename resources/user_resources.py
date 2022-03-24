14712f9d02e5616688c75cf436cc4d0b894448a6
coral-cohen <coralc12@gmail.com>
2022-03-24 14:43:34 +0200

    added resources classes to user_resources with empty mehtods

--- resources/user_resources.py
+++ resources/user_resources.py
@@ -1,1 +1,25 @@
 from flask_restful import Resource
+
+
+class SignUp(Resource):
+    def get(self):
+        pass
+
+
+class Profile(Resource):
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+
+class LogIn(Resource):
+    def post(self):
+        pass
+
+
+class LogOut(Resource):
+    def get(self):
+        pass
+    
