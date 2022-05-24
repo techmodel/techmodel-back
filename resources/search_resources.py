7528b8d03fe39d5603eb636c11a68cb0a27e0737
guy <rosenberg.guy@gmail.com>
2022-05-24 15:48:47 +0300

    removed unneeded try except

--- resources/search_resources.py
+++ resources/search_resources.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource, ResponseBase
+from flask_restful import Resource
 from flask import request
 from presenters.search import SearchEngine
 from presenters.union_user_data import union_user_data
@@ -9,12 +9,8 @@ class Search(Resource):
 
     @try_get_resource
     def post(self, user_type):
-        try:
-            body = request.json
-            search_engine = SearchEngine()
-            results = search_engine.search(body, user_type)
-            union_data = union_user_data(results)
-            print({"results": union_data})
-            return {"results": union_data}
-        except (DBError, Exception) as e:
-            return ResponseBase(str(e), 503)
+        body = request.json
+        search_engine = SearchEngine()
+        results = search_engine.search(body, user_type)
+        union_data = union_user_data(results)
+        return {"results": union_data}
