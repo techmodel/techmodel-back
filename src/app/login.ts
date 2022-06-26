f7131da4ab19ee3ebfda6be4ddc4e0f813d76cdb
Itay Houri <itay9263@gmail.com>
2022-06-27 00:04:29 +0300

    Fixed linter warning and indexed model folder

--- src/app/login.ts
+++ src/app/login.ts
@@ -2,12 +2,20 @@ import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../repos';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '../config';
+import { User, UserType } from '../models';
 
-export const login = async (userId: number) => {
+type loginResponse = {
+  userDetails: User | null;
+  isFound: boolean;
+  userToken: string;
+  userType: UserType | null;
+};
+
+export const login = async (userId: number): Promise<loginResponse> => {
   const userRepository = getCustomRepository(UserRepository);
   const user = await userRepository.findOne({ id: userId });
   if (!user) {
-    return { userDetails: null, isFound: false, userToken: null, userType: null };
+    return { userDetails: null, isFound: false, userToken: '', userType: null };
   }
   const tokenData = { userType: user.userType, userId };
   const token = sign(tokenData, JWT_SECRET, { expiresIn: '1d' });
---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+export { City } from './city';
+export { Company } from './company';
+export { Institution } from './institution';
+export { InstitutionType } from './institutionType';
+export { Language } from './language';
+export { Location } from './location';
+export { PopulationType } from './populationType';
+export { Program } from './program';
+export { Skill } from './skill';
+export { SkillToVolunteerRequest } from './skillToVolunteerRequest';
+export { User } from './user';
+export { UserType } from './userType';
+export { VolunteerRequest } from './volunteerRequest';
+export { RequestStatus } from './volunteerRequestStatus';
+export { VolunteerRequestToVolunteer } from './volunteerRequestToVolunteer';
