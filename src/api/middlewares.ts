cd83f74beb8d428489cab349a7503f284e0b2c4e
Itay Houri <itay9263@gmail.com>
2022-06-27 11:37:41 +0300

    auth middleware with user type

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -2,21 +2,25 @@ import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 import { AuthorizationError } from '../exc';
 import { JWT_SECRET } from '../config';
-import { userDecoded } from '../models';
+import { userDecoded, UserType } from '../models';
 
 const tokenValidation = (token: string) => jwt.verify(token, JWT_SECRET) as userDecoded;
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = (userType: UserType) => (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers['authorization'];
   try {
     if (!token) {
       throw new AuthorizationError('No token found');
     }
+
     const decoded = tokenValidation(token);
-    //TODO: think about what do do with decoded, like check type
 
     if (!decoded) {
       throw new AuthorizationError("Couldn't verify token");
+    }
+
+    if (userType != decoded.userType) {
+      throw new AuthorizationError('You are not authorized to perform this action');
     }
   } catch (err) {
     next(err);
