b3b8bc277f2db5303f6b154ff8c6237cf3af1c5c
hyaxia <maxvovshin@gmail.com>
2022-07-07 12:27:00 +0300

    added swagger to volunteer to request route

--- src/api/swagger/response.ts
+++ src/api/swagger/response.ts
@@ -4,4 +4,8 @@
  *   responses:
  *     UnauthorizedError:
  *       description: Access token is missing or invalid
+ *     OperationNotAllowedError:
+ *       description: Request was sent correctly but is not allowed
+ *     NotFoundError:
+ *       description: Target entity was not found
  */
--- src/api/volunteerRequest.ts
+++ src/api/volunteerRequest.ts
@@ -70,6 +70,37 @@ router.post(
   }
 );
 
+/**
+ * @openapi
+ * paths:
+ *   /api/v1/volunteer-requests/{requestId}/volunteers/{volunteerId}:
+ *     delete:
+ *       summary: Deletes the mapping of the volunteer to the target volunteer request
+ *       security:
+ *         - bearerAuth: []
+ *       responses:
+ *         '200':
+ *           description: Volunteer has been deleted
+ *         '401':
+ *           $ref: '#/components/responses/UnauthorizedError'
+ *         '404':
+ *           $ref: '#/components/responses/NotFoundError'
+ *         '422':
+ *           $ref: '#/components/responses/OperationNotAllowedError'
+ *       parameters:
+ *         - in: path
+ *           name: requestId
+ *           schema:
+ *           type: number
+ *           required: true
+ *           description: volunteer request id the mapping of the volunteer is done to
+ *         - in: path
+ *           name: volunteerId
+ *           schema:
+ *           type: string
+ *           required: true
+ *           description: volunteer id the mapping of the volunteer is done to
+ */
 router.delete(
   '/:requestId/volunteers/:volunteerId',
   authMiddleware(UserType.VOLUNTEER),
