e03f18490ab87c9ba5fe3b64ecb82e603db07f4a
Itay Houri <itay9263@gmail.com>
2022-07-11 19:23:13 +0300

    fixed linter warnings

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,16 +1,9 @@
 require('dotenv/config');
-import logger from '../src/logger';
-import { Response } from 'superagent';
-import sinon, { SinonSandbox } from 'sinon';
 import { expect } from 'chai';
+import sinon, { SinonSandbox } from 'sinon';
 import request from 'supertest';
+import logger from '../src/logger';
 import app from '../src/server/server';
-import { API_PREFIX } from '../src/config';
-import * as core from 'express-serve-static-core';
-
-function getToApp(app: core.Express, path: string): Promise<Response> {
-  return request(app).get(`${API_PREFIX}${path}`);
-}
 
 describe('api', function() {
   let sandbox: SinonSandbox = (null as unknown) as SinonSandbox;
