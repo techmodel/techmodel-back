c3caa9203fd535c64862376c6aeaabfa4dc0ef9f
Itay Houri <itay9263@gmail.com>
2022-07-11 19:56:31 +0300

    Added update-info tests

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -2,79 +2,80 @@ import { expect } from 'chai';
 import sinon, { SinonSandbox } from 'sinon';
 import logger from '../src/logger';
 import { removeSeed, seed } from './seed';
-import { volunteer1, programCoordinator1, programManager1 } from './mock';
+import { volunteer1, programCoordinator1, programManager1, program1, company1, institution1, location1, city1 } from './mock';
 import { createTestJwt, HTTPError } from './setup';
 import app from '../src/server/server';
 import request from 'supertest';
 import { userRepository } from '../src/repos';
 import { User } from '../src/models';
 
-describe('user', function() {
+describe('user', function () {
   let sandbox: SinonSandbox = (null as unknown) as SinonSandbox;
 
-  this.beforeEach(async function() {
+  this.beforeEach(async function () {
     sandbox = sinon.createSandbox();
     // disable logging
     sandbox.stub(logger);
     // seed db
     await seed({
-      users: [volunteer1, programCoordinator1, programManager1]
+      programs: [program1],
+      users: [programManager1]
     });
   });
 
   describe('updateUserInfo', async () => {
     it('returns 422 when volunteer tries to update programId', async () => {
       const volunteer1Jwt = createTestJwt(volunteer1);
-      const newUserInfo = {
+      const userInfo = {
         programId: 1
       };
       const res = await request(app)
-        .put(`/api/v1/users/update-info`)
+        .put(`/api/v1/user/update-info`)
         .set('Authorization', `Bearer ${volunteer1Jwt}`)
-        .send(newUserInfo);
+        .send({ userInfo });
       expect(res.status).to.eq(422);
       expect((res.error as HTTPError).text).to.eq(`Can't update inaccessible user info`);
     });
 
     it('returns 422 when coordinator tries to update companyId', async () => {
       const coordinatorJwt = createTestJwt(programCoordinator1);
-      const newUserInfo = {
+      const userInfo = {
         companyId: 1
       };
       const res = await request(app)
-        .put(`/api/v1/users/update-info`)
+        .put(`/api/v1/user/update-info`)
         .set('Authorization', `Bearer ${coordinatorJwt}`)
-        .send(newUserInfo);
+        .send({ userInfo });
       expect(res.status).to.eq(422);
       expect((res.error as HTTPError).text).to.eq(`Can't update inaccessible user info`);
     });
 
     it('returns 422 when manager tries to update institutionId', async () => {
       const managerJwt = createTestJwt(programManager1);
-      const newUserInfo = {
+      const userInfo = {
         institutionId: 1
       };
       const res = await request(app)
-        .put(`/api/v1/users/update-info`)
+        .put(`/api/v1/user/update-info`)
         .set('Authorization', `Bearer ${managerJwt}`)
-        .send(newUserInfo);
+        .send({ userInfo });
       expect(res.status).to.eq(422);
       expect((res.error as HTTPError).text).to.eq(`Can't update inaccessible user info`);
     });
 
     it('Successfully updates user', async () => {
       const managerJwt = createTestJwt(programManager1);
-      const newUserInfo = {
+      const userInfo = {
         programId: 1,
         firstName: `first`
       };
       const res = await request(app)
-        .put(`/api/v1/users/update-info`)
+        .put(`/api/v1/user/update-info`)
         .set('Authorization', `Bearer ${managerJwt}`)
-        .send(newUserInfo);
+        .send({ userInfo });
       expect(res.status).to.eq(204);
       const managerUser = (await userRepository.findOneBy({ id: programManager1.id })) as User;
-      expect(managerUser.programId).to.eq(`1`);
+      expect(managerUser.programId).to.eq(1);
       expect(managerUser.firstName).to.eq(`first`);
     });
   });
