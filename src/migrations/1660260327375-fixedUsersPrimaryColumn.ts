37e50e2e09066e00a65cd3a819cd826e20657651
hyaxia <hyaxia@gmail.com>
2022-08-12 10:47:39 +0300

    fix migration `fixedUsersPrimaryColumn` - removed database name reference

--- src/migrations/1660260327375-fixedUsersPrimaryColumn.ts
+++ src/migrations/1660260327375-fixedUsersPrimaryColumn.ts
@@ -4,12 +4,12 @@ export class FixedUsersPrimaryColumn1660260327375 implements MigrationInterface 
   name = 'fixedUsersPrimaryColumn1660260327375';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`EXEC sp_rename "master.dbo.users.PK_users", "id"`);
-    await queryRunner.query(`EXEC sp_rename "master.dbo.users.PK_0a21a5d1be8adf5ed4cbb40ec84", "PK_users"`);
+    await queryRunner.query(`EXEC sp_rename "dbo.users.PK_users", "id"`);
+    await queryRunner.query(`EXEC sp_rename "dbo.users.PK_0a21a5d1be8adf5ed4cbb40ec84", "PK_users"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`EXEC sp_rename "master.dbo.users.PK_users", "PK_0a21a5d1be8adf5ed4cbb40ec84"`);
-    await queryRunner.query(`EXEC sp_rename "master.dbo.users.id", "PK_users"`);
+    await queryRunner.query(`EXEC sp_rename "dbo.users.PK_users", "PK_0a21a5d1be8adf5ed4cbb40ec84"`);
+    await queryRunner.query(`EXEC sp_rename "dbo.users.id", "PK_users"`);
   }
 }
