d4b9f228c355860d4154740957607f7dd0cde3ff
hyaxia <hyaxia@gmail.com>
2022-08-13 17:15:41 +0300

    changed `/api/v1/programs/volunteer-requests` path to `/api/v1/programs/{programId}/volunteer-requests`

--- src/api/program.ts
+++ src/api/program.ts
@@ -35,7 +35,7 @@ router.get('/', async (req: Request, res: Response, next: NextFunction) => {
 /**
  * @openapi
  * paths:
- *   /api/v1/programs/volunteer-requests:
+ *   /api/v1/programs/{programId}/volunteer-requests:
  *    get:
  *     operationId: getObject
  *     security:
@@ -55,16 +55,27 @@ router.get('/', async (req: Request, res: Response, next: NextFunction) => {
  *         schema:
  *           type: string
  *           required: false
+ *       - in: path
+ *         name: programId
+ *         schema:
+ *         type: number
+ *         required: true
+ *         description: program id
  */
+// TODO: add tests for this route
 router.get(
-  '/volunteer-requests',
+  '/:programId/volunteer-requests',
   authMiddleware([UserType.PROGRAM_COORDINATOR, UserType.PROGRAM_MANAGER]),
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { programId, institutionId } = (req as DecodedRequest).userDecoded;
+      const { programId: userProgramId, institutionId } = (req as DecodedRequest).userDecoded;
       const startDate = req.query.startDate as string | undefined;
-      if (!programId) throw new AuthorizationError('No program found');
-      res.json(await getVolunteerRequestsOfProgram(programId, institutionId, startDate));
+      if (!userProgramId) throw new AuthorizationError('No program found');
+      const pathProgramId = parseInt(req.params.programId, 10);
+      if (pathProgramId != userProgramId) {
+        throw new AuthorizationError('Trying to access another program data');
+      }
+      res.json(await getVolunteerRequestsOfProgram(userProgramId, institutionId, startDate));
     } catch (e) {
       next(e);
     }
