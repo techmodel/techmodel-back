719042d79173de9b90b0c709f0156d7caded4141
Itay Houri <itay9263@gmail.com>
2022-12-18 23:11:03 +0200

    Changed institution type again loll

--- src/models/institutionType.ts
+++ src/models/institutionType.ts
@@ -1,8 +1,8 @@
 export enum InstitutionType {
-  PRIMARY_SCHOOL = 'PRIMARY_SCHOOL',
-  ELEMENTARY_SCHOOL = 'elementary_school',
-  HIGH_SCHOOL = 'high_school',
-  MOVEMENT = 'movement'
+  ELEMENTARY = 'elementary',
+  HIGH = 'high',
+  MIDDLE = 'middle',
+  MOVEMENTS = 'movemnets'
   // TODO: add the rest of the options
   // 'יסודי',
   // 'ביניים',
